8c9e51bb9a6254600b6950cd7909360eb37c9958
emanuele23dev <emanuele.meggiarin@libero.it>
2024-10-16 15:36:26 +0200

    end snack 3

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,24 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+// METODO CICLO FOR 
+
+const newNumbers = [];
+
+for (let i = 0; i < numbers.length; i++) {
+    let numero = numbers[i]
+    numero = numero +1;
+    newNumbers.push(numero)
+}
+console.log(newNumbers);
+
+
+// METODO FOR EACH 
+
+// numbers.forEach((numero) => {
+//   const nuovoNumero = numero + 1;
+//   newNumbers.push(nuovoNumero);
+// });
+
+// console.log(newNumbers);
