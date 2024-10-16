3a0560ebfcf58c9ef03b8b9bfc60a43c1d06b58e
emanuele23dev <emanuele.meggiarin@libero.it>
2024-10-16 14:57:09 +0200

    end first snack

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,9 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
+names.forEach((nomi) => {
+    console.log(nomi);
+})
+    
+
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
