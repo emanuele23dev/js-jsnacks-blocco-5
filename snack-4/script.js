67b1b9bf9aba819f842d85f3e9a350211ca377cc
emanuele23dev <emanuele.meggiarin@libero.it>
2024-10-16 15:54:30 +0200

    end snack 4

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,8 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const nomiAutori = posts.map((nomi) => {
+  return nomi.author
+})
+console.log(nomiAutori);
